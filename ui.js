c236154633076a4ce138538ca6acec8305a44e34
akinnevmo-gif <sokpahakinsaye@gmail.com>
2026-03-06 23:31:38 +0000

    Create ui.js

---
+++ ui.js
@@ -0,0 +1,8 @@
+document.getElementById("radius").innerText =
+IGC_CONFIG.compassRadius
+
+document.getElementById("needle").innerText =
+IGC_CONFIG.needleLength
+
+document.getElementById("declination").innerText =
+IGC_CONFIG.declination
